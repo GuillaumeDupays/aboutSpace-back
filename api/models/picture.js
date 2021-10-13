8624347d7298b64b676ce82f094bf858ec3c0e27
Guillaume Dupays <guillaumedupays@gmail.com>
2021-10-13 17:10:01 +0200

    just a commentary to test source tree

--- api/models/picture.js
+++ api/models/picture.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+// a Schema to have a model of data like sql databases, front app must have a correct object
 const pictureElementsSchema = new mongoose.Schema({
 	nasaId: String,
 	title: String,
